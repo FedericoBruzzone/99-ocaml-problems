1d622da8f2dbc78d1593cedd88d1bbece100a37d
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-10-14 15:21:11 +0200

    Remove old folder
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- old/beginner/binary_tree/count_the_leaves_of_a_binary_tree.ml
+++
@@ -1,4 +0,0 @@
-(* TODO *)
-
-count_leaves Empty;;
-
--- old/beginner/lists/insert_an_element_at_a_given_position_into_a_list.ml
+++
@@ -1,7 +0,0 @@
-let rec insert_at el count = function
-    | [] -> [ el ]
-    | h :: t as l -> if count = 0 then el :: l
-                else h :: insert_at el (count - 1) t
-;;
-
-insert_at "alfa" 1 ["a"; "b"; "c"; "d"];;
